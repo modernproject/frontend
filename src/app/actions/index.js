c808ec633624882ca8873295094c8b5af1c1f91a
Dylan Stein <djstein@ncsu.edu>
2018-03-31 17:43:05 -0400

    dropdown removed when signout clicked

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -241,7 +241,6 @@ export function logoutAction() {
       .then(response => {
         removeJWTCookie()
         removeJWTFromAxios()
-        dispatch(updateDropDown())
         dispatch(userClear())
         dispatch(push('/'))
       })
